dde089e17c262f423f8fcd76f7d505b2d8538327
Stacy Nowak <nowak.stacy@gmail.com>
2019-07-06 17:26:23 -0700

    expanding app decriptions on image link

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+    $(".app-desc").hide();
+
+    $("#img1").on("click", function(){$("#desc1").toggle()});
+    $("#img2").on("click", function(){$("#desc2").toggle()});
+    $("#img3").on("click", function(){$("#desc3").toggle()});
+    $("#img4").on("click", function(){$("#desc4").toggle()});
+    $("#img5").on("click", function(){$("#desc5").toggle()});
+    $("#img6").on("click", function(){$("#desc6").toggle()});
+
     $("#pagepiling").pagepiling({
         normalScrollElements: '.pp-scrollable'
     });
